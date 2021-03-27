f791ffe76b21c776b102bb97cddbb0cfe24cd91f
bilalcapripio <bilal.capripio@gmail.com>
2021-03-27 13:09:35 +0500

    Finally Run the Code [Testing]

--- PWA-STUFF-master/sw.js
+++ PWA-STUFF-master/sw.js
@@ -1,77 +1,77 @@
-self.addEventListener('install', e =>{
-    // console.log("Install",e);
-    e.waitUntill(
-        caches.open("static").then(cache => {
-                return cache.addAll(
-                    "./",
-                    "style.css",
-                    "icon/android-icon-192x192-dunplab-manifest-42526.jpg"
-                    )
-            }
-        )
-    )
-})
+// self.addEventListener('install', e =>{
+//     // console.log("Install",e);
+//     e.waitUntill(
+//         caches.open("static").then(cache => {
+//                 return cache.addAll(
+//                     "./",
+//                     "style.css",
+//                     "icon/android-icon-192x192-dunplab-manifest-42526.jpg"
+//                     )
+//             }
+//         )
+//     )
+// })
 //Fetch
-self.addEventListener("fetch", e => {
-    console.log(`Intersect fetch request for: ${e.request.url}`);
+// self.addEventListener("fetch", e => {
+    // console.log(`Intersect fetch request for: ${e.request.url}`);
     // e.respondWith(
     //     caches.match(e.request).then(response=>{
     //         return response || fetch(e.request);
     //     })
     // )
-})
-
+// })
+// 
 //second method ovi
-    // var app='v1.00';
+    var app='v1.00';
 
-    // var files=[
-    //     "./",
-    //     "index.html",
-    //     "style.css",
-    //     "icon/android-icon-192x192-dunplab-manifest-42526.jpg"
-    // ]
+    var files=[
+        "./",
+        "index.html",
+        "style.css",
+        "icon/android-icon-192x192-dunplab-manifest-42526.jpg"
+    ]
 
     //     //Install
-    // self.addEventListener('install', event =>{
-    //     // console.log("Install",e);
-    //     event.waitUntill(
-    //         caches.open("app")
-    //         .then(cache => {
-    //             return cache.addAll(files)
-    //             .catch(error => {
-    //                 console.error("Error adding files to", error);
-    //             })
-    //             })
-    //     )
-    //     console.info('SW Installed');
-    //     // self.skipWaiting();
-    // })
+    self.addEventListener('install', event =>{
+        // console.log("Install",e);
+        event.waitUntill(
+            caches.open("app")
+            .then(cache => {
+                return cache.addAll(files)
+                .catch(error => {
+                    // console.error("Error adding files to", error);
+                })
+                })
+        )
+        console.info('SW Installed');
+        // self.skipWaiting();
+    })
 
     //   //Activate
-    // self.addEventListener('activate', event=>{
-    //     event.waitUntill(
-    //     caches.keys()
-    //     .then(cacheName=>{
-    //         return Promise.all(
-    //             cacheName.map(cache=>{
-    //                 if(cache !== app){
-    //                     console.info("Deleting Old Cache", cache);
-    //                     return caches.delete(cache);
-    //                 }
-    //             })
-    //         )
-    //     })
-    // )
-    // // return self.clients.claim();
-    // })  
+    self.addEventListener('activate', event=>{
+        event.waitUntill(
+        caches.keys()
+        .then(cacheName=>{
+            return Promise.all(
+                cacheName.map(cache=>{
+                    if(cache !== app){
+                        // console.info("Deleting Old Cache", cache);
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    )
+    // return self.clients.claim();
+    })  
 
     //   ///Fetch
-    // self.addEventListener('fetch',event => {
-    //     console.info("SW fetch",event.request.url);
-    //     event.respondWith(
-    //         caches.match(event.request)
-    //         .then(response => {
-    //             return response || fetch(event.request);
-    //         })
-    //     )
-    // })
+    self.addEventListener('fetch',event => {
+        // console.info("SW fetch",event.request.url);
+        event.respondWith(
+            caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+        )
+    })
